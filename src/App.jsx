dde4a9d5ed440d73f9ccd0a6dc9d040a20ccbce4
AnnnaPiotrowska <piotrowskaannamilena@gmail.com>
2025-11-13 21:23:36 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// App.js
+import { useState } from 'react';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
+function App() {
+
+  const [page, setPage] = useState('home');
+
+  // TODO: How does the Layout component know which page to render?
+// we have state, passing this onto the layout, so have access to the state, can control this state,
+// 
+  return (
+    <Layout selectedPage={page} onSetPage={setPage}>
+
+      {page === 'home' && <HomePage />}
+      {page === 'about' && <AboutPage />}
+      {page === 'contact' && <ContactPage />}
+
+    </Layout>
+  );
+}
+
+export default App;
